8332c954569bc06fe056bdf9eba5553e7a4615b5
Sahaj <shakyasahaj10@gmail.com>
2024-03-16 10:21:19 +0545

    Sortcities completed

--- SortCities.java
+++ SortCities.java
@@ -1,18 +1,24 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Inputs{
     public Inputs(){
 
         String[] cities = {"Kathmandu", "Lalitpur", "Delhi", "Georgia", "Karachi"};
 
-        System.out.println("Unsorted list:\n");
+        System.out.println("Unsorted list:");
         for(int i=0;i<5;i++){
-            System.out.println(cities[i] + "\n");
+            System.out.println((i + 1) + ". " + cities[i]);
         }
     }
 
     public void Sorted(){
+        String[] cities = {"Kathmandu", "Lalitpur", "Delhi", "Georgia", "Karachi"};
+        Arrays.sort(cities);
         
+        System.out.println("\nSorted List: ");
+        for(int i = 0; i < 5; i++){
+            System.out.println((i + 1) + ". " + cities[i]);
+        }
     }
 }
 
@@ -20,5 +26,6 @@ public class SortCities {
     public static void main(String[] args) {
         Inputs A = new Inputs();
         A.Sorted();
+
     }
 }
